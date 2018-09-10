59c112c72a9074296ecbafcc10345f7afd15ea3d
张阳 <2523990559@qq.com>
2018-09-10 16:15:56 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+Vue.use(iView);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +14,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import FirstPage from '@/page/FirstPage/index.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '首页',
+      component: FirstPage
     }
   ]
-})
+});
